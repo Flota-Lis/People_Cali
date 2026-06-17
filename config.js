ab53e0c8e6faf925169228a36e19d66597df0136
Flota-Lis <supervisorflt.girardota@lis.com.co>
2026-06-17 11:31:53 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Este archivo contiene las llaves de conexión a Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBZBGvHc3dw7OTh6Uo0DqTZT9mHBgr6ETM",
+  authDomain: "portal-people-7093f.firebaseapp.com",
+  projectId: "portal-people-7093f",
+  storageBucket: "portal-people-7093f.firebasestorage.app",
+  messagingSenderId: "1050558890884",
+  appId: "1:1050558890884:web:17f70beb632e001bfc53d1"
+};
